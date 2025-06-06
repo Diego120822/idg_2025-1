12e29c8d04bd1c9ddf7b8d059baa7d2afee766d4
Diego <diego.llanculef@usach.cl>
2025-06-06 16:49:26 -0400

    commit clase 06062025

--- SCRIPTS/clusterCENSORM.R
+++ SCRIPTS/clusterCENSORM.R
@@ -1,26 +1,27 @@
 # =============================================================================
 # 1) INSTALAR PAQUETES (solo una vez)
 # =============================================================================
-install.packages(c("DBI", "RPostgres", "sf", "ggplot2", "cowplot", "biscale"))
+install.packages(c("DBI", "RPostgres", "sf", "ggplot2", "cowplot", "ggfortify", "factoextra"))
 
 # =============================================================================
 # 2) CARGAR LIBRERÍAS NECESARIAS
 # =============================================================================
+library(factoextra)
+library(ggfortify)
 library(DBI)
 library(RPostgres)
 library(sf)
 library(ggplot2)
 library(cowplot)
-library(biscale)
 
 # =============================================================================
 # 3) CONFIGURAR CONEXIÓN A BASE DE DATOS
 # =============================================================================
 db_host    = "localhost"
 db_port    = 5432
-db_name    = "CENSO_QUINTA_REGION_2017" 
+db_name    = "censo_rm_2017" 
 db_user    = "postgres"
-db_password = "POSTGRES"
+db_password = "postgres"
 
 con = dbConnect(
   Postgres(),
@@ -39,7 +40,7 @@ con = dbConnect(
 #  Por ejemplo, podría haber una columna 'manzana_id' en la tabla 'personas'
 #  o una tabla de 'asignación' que vincule personas a manzanas.
 
-sql_indicadores_manzana = "
+sql_indicadores = "
 SELECT
   z.geocodigo::double precision AS geocodigo,
   c.nom_comuna,
@@ -73,4 +74,38 @@ ORDER BY ptje_esc_mayor_12 DESC;
 
 "
 
-df_indicadores_manzana = dbGetQuery(con, sql_indicadores_manzana)
+# Ejecutar consulta y importar resultados a data.frame en R
+df_indicadores = dbGetQuery(con, sql_indicadores)
+
+# ====================================================================================
+# 5) Seleccionar variables y escarlatas
+# =====================================================================================
+vars_clusters = df_indicadores[,c("ptje_migrantes", "ptje_esc_mayor_12", "ptje_adulto_mayor")]
+
+# Se escalan las variables
+vars_scaled = scale(vars_clusters)
+
+# 5) Metodo del codo, para elegir k
+# visualiza la suma de cuadrados dentro del cluster (wss) para varios k
+fviz_nbclust(vars_scaled, kmeans, method = "wss") +
+  labs(tittle = "Método del codo", x = "Número de clusters (k)", y = "wss")
+
+# 5) K-means
+set.seed(123)  # para reproducibilidad
+km = kmeans(vars_scaled, centers = 4, nstart = 25)
+
+df_indicadores$cluster = as.factor(km$cluster)
+
+# Escolaridad v/s Migración
+ggplot(df_indicadores_manzana, aes(x = ptje_esc_mayor_12, y = ptje_migrantes, color = cluster)) + 
+  geom_point(size = 2) +
+  labs(tittle = "escolaridad v/s migrantes",
+       x = "% población con 12 años de escolaridad",
+       y = "% población migrante") + 
+  theme_minimal()
+
+# C1: Escolaridad madia-baja, migración baja, pablación joven
+# C2: Escolaridad media-alta , alta migración, población joven
+# C3: Escolaridad alta, migración media baja, población mayor
+# C4: Escolaridad baja, migración baja, población mayor
+
